f0b216392993e21e3c16f6aae93318ec604f530f
AD <65587162+algodesk-io@users.noreply.github.com>
2021-09-29 16:27:35 +0530

    app update

--- packages/fundstack-contracts-v1/src/contracts/utils/utils.py
+++ packages/fundstack-contracts-v1/src/contracts/utils/utils.py
@@ -1,36 +1,43 @@
 from pyteal import *
 
 
+@Subroutine(TealType.uint64)
 def getAppId():
     return Txn.application_id()
 
 
+@Subroutine(TealType.uint64)
 def getTxnAction():
     return Txn.on_completion()
 
 
+@Subroutine(TealType.uint64)
 def isCreate():
     return getAppId() == Int(0)
 
 
+@Subroutine(TealType.uint64)
 def isUpdate():
     return getTxnAction() == OnComplete.UpdateApplication
 
 
+@Subroutine(TealType.uint64)
 def isDelete():
     return getTxnAction() == OnComplete.DeleteApplication
 
 
+@Subroutine(TealType.uint64)
 def isOptIn():
     return getTxnAction() == OnComplete.OptIn
 
 
+@Subroutine(TealType.uint64)
 def isCloseOut():
     return getTxnAction() == OnComplete.CloseOut
 
 
 @Subroutine(TealType.uint64)
-def getAssetMicros(foreignAsset):
+def getAssetMicros(foreignAsset: Expr) -> TealType.uint64:
     assetDecimal = AssetParam.decimals(foreignAsset)
     micros = Seq([
         assetDecimal,
@@ -40,15 +47,23 @@ def getAssetMicros(foreignAsset):
     return micros
 
 
+@Subroutine(TealType.uint64)
 def blockOperation():
     return Int(0) == Int(1)
 
 
+@Subroutine(TealType.uint64)
 def allowOperation():
     return Int(1) == Int(1)
 
+
+@Subroutine(TealType.uint64)
 def microAlgoToAlgo(amount):
     return amount / Int(1000000)
 
+
+@Subroutine(TealType.uint64)
 def AlgoToMicroAlgo(amount):
     return amount * Int(1000000)
+
+
