84d8d5828e1a55f2038c376e3a90349eb97f0d12
AD <65587162+algodesk-io@users.noreply.github.com>
2021-10-16 14:38:05 +0530

    app update

--- packages/website/src/utils/theme.ts
+++ packages/website/src/utils/theme.ts
@@ -25,7 +25,7 @@ export const CustomButton = withStyles((theme) => ({
     containedPrimary: {
         boxShadow: "none",
         background: "linear-gradient(56.21deg, #60DD8B -43.1%, #0BB68C 132.97%)",
-        padding: "12px 24px"
+        padding: "12px 24px",
     },
     outlinedPrimary: {
         padding: "12px 24px",
