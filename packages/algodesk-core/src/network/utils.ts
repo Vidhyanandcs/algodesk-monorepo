0074c44795ebd554fc99cb8e0097dea627b6f48a
AD <65587162+algodesk-io@users.noreply.github.com>
2022-03-16 19:08:50 +0530

    node update

--- packages/algodesk-core/src/network/utils.ts
+++ packages/algodesk-core/src/network/utils.ts
@@ -1,9 +1,9 @@
 import {Network} from "./network";
 import {LOCAL_STORAGE, NETWORKS} from "../constants";
 
-export const testnet = new Network(NETWORKS.TESTNET, 'TestNet', 'https://api.testnet.algoexplorer.io', 'https://api.testnet.algoexplorer.io/idx2');
+export const testnet = new Network(NETWORKS.TESTNET, 'TestNet', 'https://testnet-api.algonode.cloud', 'https://testnet-idx.algonode.cloud');
 export const betanet = new Network(NETWORKS.BETANET, 'BetaNet', 'https://api.betanet.algoexplorer.io', 'https://api.betanet.algoexplorer.io/idx2');
-export const mainnet = new Network(NETWORKS.MAINNET, 'MainNet', 'https://api.algoexplorer.io', 'https://api.algoexplorer.io/idx2');
+export const mainnet = new Network(NETWORKS.MAINNET, 'MainNet', 'https://mainnet-api.algonode.cloud', 'https://mainnet-idx.algonode.cloud');
 
 export function getNetwork(name: string): Network{
     if (name == NETWORKS.TESTNET) {
